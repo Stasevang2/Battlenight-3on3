af7c52a574de4231a413d337663f1d87366939ae
Stasevang2 <mail.nummer10@gmail.com>
2026-07-13 07:45:21 +0200

    Update leaderboard.tsx

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-missing-dom';
+import { useNavigate } from 'react-router-dom';
 import BottomNav from '../components/BottomNav';
 import '../styles/leaderboard.css';
 
